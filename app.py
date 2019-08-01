d4dbb6ac6526263dca88e1fd020eeadb42b8435f
pashtet <pashtetbezd@gmail.com>
2019-08-02 04:25:52 +0500

    test

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ import time
 
 import redis
 from flask import Flask
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 cache = redis.Redis(host='redis', port=6379)
 
 
@@ -23,3 +28,7 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
