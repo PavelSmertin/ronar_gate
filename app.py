86c579cc9cc2e5d05e4f7d31b0f5626b1e876276
pashtet <pashtetbezd@gmail.com>
2019-08-04 00:04:43 +0500

    get chat id

--- app.py
+++ app.py
@@ -103,6 +103,8 @@ def sub_stream():
     for item in pubsub.listen():
         if isinstance(item['data'], bytes):
             socketio.emit('transaction', get_emit(item['data']))
+            # if is_command(item['data']):
+            #     bot.send_message('@my_channel_name', item['data'])
 
 def get_emit(msg):
     return { 
@@ -111,21 +113,20 @@ def get_emit(msg):
         'data': " ".join(["{:02x}".format(x).upper() for x in msg[2:]])
     }
 
+def is_command(msg):
+    return True
 
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+    bot.reply_to(message, message.chat.id)
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, message.chat.id)
 
 
 def start_bot():
